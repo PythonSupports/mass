26c149429a852bc4d38e6a50d38eddc5ac1e582f
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-04-24 10:30:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class Bot (Client):
         logging.info(f"✅  with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}. ✅")
 
 
-        await self.send_message(Config.OWNER, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")
+        await self.send_message(Config.OWNER, f"** Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")
 
         
     async def stop(self, *args):
